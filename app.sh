146f39c5b783950a034f450ab3c8834ce1049811
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-20 18:09:07 +0000

    Update app.shsss

--- app.sh
+++ app.sh
@@ -193,14 +193,15 @@ function iniciar(){
 esperar instalar "${WHITE}Instalando..." " ${WHITE} Instalado!"
 
 read -n 1 -r -s -p "Press any key to continue..."
-clear
+
 
 
 step "Update:"
-    #try sudo apt update -y
-    #try sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
-    #try sudo iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
-    #try sudo iptables -A PREROUTING -t nat -p tcp --dport 8080 -j REDIRECT --to-port 80
-    #try sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8080
-    esperar "try sudo apt update -y" "Atualizando..." " ${WHITE} Atualizado!"
+    try sudo apt update -y
+next
+
+read -n 1 -r -s -p "Press any key to continue..."
+
+step "Update:"
+    try esperar "sudo apt update -y" "Atualizando..." " ${WHITE} Atualizado!"
 next
