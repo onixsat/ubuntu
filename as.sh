f2275610b71a5e371561545dfbde7ff6af9144f3
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-20 22:59:26 +0000

    Create as.sh

---
+++ as.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+#./etc/init.d/functions
+BOOTUP=color
+RES_COL=60
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+function draw_spinner(){
+    # shellcheck disable=SC1003
+    local -a marks=( '/' '-' '\ ' '|' )
+    local i=0
+    delay=${SPINNER_DELAY:-0.25}
+    message=${1:-}
+    while :; do
+        printf '%s\r' "${marks[i++ % ${#marks[@]}]} ${message}"
+        sleep "${delay}"
+    done
+}
+function start_loading(){
+    message=${1:-}                                # Set optional message
+    draw_spinner "${message}" &                   # Start the Spinner:
+    SPIN_PID=$!                                   # Make a note of its Process ID (PID):
+    declare -g SPIN_PID
+    # shellcheck disable=SC2312
+    trap stop_loading $(seq 0 15)
+}
+function stop_loading(){
+    if [[ "${SPIN_PID}" -gt 0 ]]; then
+        kill -9 "${SPIN_PID}" > /dev/null 2>&1;
+    fi
+    SPIN_PID=0
+    printf '\033[2K'
+}
+echo_success() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+    echo -n $"  OK  "
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 0
+}
+echo_failure() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+    echo -n $"FAILED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+}
+echo_passed() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+    echo -n $"PASSED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+}
+echo_warning() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+    echo -n $"WARNING"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+} 
+. ./functions.sh
+
+step() {
+    echo -n "$@"
+
+    STEP_OK=0
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+try() {
+    # Check for `-b' argument to run command in the background.
+    local BG=
+
+    [[ $1 == -b ]] && { BG=1; shift; }
+    [[ $1 == -- ]] && {       shift; }
+
+    # Run the command.
+    if [[ -z $BG ]]; then
+        "$@"
+    else
+        "$@" &
+    fi
+
+    # Check if command failed and update $STEP_OK if so.
+    local EXIT_CODE=$?
+
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        STEP_OK=$EXIT_CODE
+        [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+
+        if [[ -n $LOG_STEPS ]]; then
+            local FILE=$(readlink -m "${BASH_SOURCE[1]}")
+            local LINE=${BASH_LINENO[0]}
+
+            echo "$FILE: line $LINE: Command \`$*' failed with exit code $EXIT_CODE." >> "$LOG_STEPS"
+        fi
+    fi
+
+    return $EXIT_CODE
+}
+next() {
+    [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
+    [[ $STEP_OK -eq 0 ]]  && echo_success || echo_failure
+    echo
+
+    return $STEP_OK
+}
+step "etapa1"
+try sudo update
+next
+step "etapa2"
+try sudo mv oi oi
+next
+step "etapa3"
+try sudo apt install curl
+next
