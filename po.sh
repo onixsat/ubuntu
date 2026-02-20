2e681dcc7e1718d4b44d467c23598f4e48a0ed25
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-20 23:40:37 +0000

    Update po.sh

--- po.sh
+++ po.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
-
+echo_success() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+    echo -n $"  OK  "
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 0
+}
+echo_failure() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+    echo -n $"FAILED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+}
 function check_previous {
+        start_time2=$(date +%s%3N)
         RESULT=$?
         if [ $RESULT -eq 0 ]; then
-        echo "$1: success"
+
+        end_time2=$(date +%s%3N)
+        duration_ms2=$((end_time2 - start_time2))
+        
+        echo -e "$1 Execution: $duration_ms2"
+        echo_success()
         else
         echo "$1: failed"
+        echo_failure()
         fi
+
 }
 
 echo "wwe"
 check_previous "echo"
 
+check_previous "sudo apt update"
 ls - l
 check_previous "ls"
