7f0481a8e2504c732066f63d8fbedf50c3d3acdb
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-20 21:50:04 +0000

    Update functions.shjjj

--- functions.sh
+++ functions.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #./etc/init.d/functions
+
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
+
 BOOTUP=color
 RES_COL=60
 MOVE_TO_COL="echo -en \\033[${RES_COL}G"
