2446291ffbf4c4284168587f8b4f28a2ebd3a0ce
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-21 16:36:34 +0000

    Create 6.sh

---
+++ 6.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+. /etc/init.d/functions
+BOOTUP=color
+RES_COL=60
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+echo_success() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+    echo -n $"  OK  "
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 0
+}
+echo_failure() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+    echo -n $"FAILED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+}
+echo_passed() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+    echo -n $"PASSED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+}
+echo_warning() {
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+    echo -n $"WARNING"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo -ne "\r"
+    return 1
+} 
+step() {
+    echo -n "$@"
+    STEP_OK=0
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+try() { # Check for `-b' argument to run command in the background.
+    local BG=
+    [[ $1 == -b ]] && { BG=1; shift; }
+    [[ $1 == -- ]] && {       shift; }
+    # Run the command.
+    if [[ -z $BG ]]; then
+        "$@"
+    else
+        "$@" &
+    fi
+    # Check if command failed and update $STEP_OK if so.
+    local EXIT_CODE=$?
+
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        STEP_OK=$EXIT_CODE
+        [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+
+        if [[ -n $LOG_STEPS ]]; then
+            local FILE=$(readlink -m "${BASH_SOURCE[1]}")
+            local LINE=${BASH_LINENO[0]}
+
+            echo "$FILE: line $LINE: Command \`$*' failed with exit code $EXIT_CODE." >> "$LOG_STEPS"
+        fi
+    fi
+
+    return $EXIT_CODE
+}
+next() {
+    [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
+    [[ $STEP_OK -eq 0 ]]  && echo_success || echo_failure
+    echo
+    return $STEP_OK
+}
+
+function add(){
+    arg1=$1
+    arg2=$2
+    arg3=$3
+
+    step "${arg1}"
+        if [ -z "$arg3" ] then
+            try ${arg2} >/dev/null 2>&1 &
+        else         
+           try ${arg2}
+        fi
+    next
+
+}
+add "Atualizar" "sudo apt update" "null"
+
+add "Instalar dnf" "sudo apt install dnf" "null"
+add "Instalar initscripts" "sudo apt dnf install -y initscripts" "null"
+
+#read -n 1 -s -p "Press any key to continue 1"
+
+add "Atualizar" "sudo apt update"
+
+#read -n 1 -s -p "Press any key to continue 2 "
+
+step "Atualizar:"
+    try sudo apt update >/dev/null 2>&1 &
+next
+
+step "Instalar: dos2unix"
+    try sudo apt install dos2unix -y >/dev/null 2>&1 &
+next
+
+step "Instalar: nginx"
+    try sudo apt install nginx nginx-full -y >/dev/null 2>&1 &
+next
+
+step "Instalar: Firewalls"
+    try sudo apt install ufw -y >/dev/null 2>&1 &
+    try sudo apt install iptables-persistent -y >/dev/null 2>&1 &
+next
+
+step "Instalar: Firewalls"
+    try echo 'This is a test' > data.txt
+    try mv file.txt data.txt
+    try echo 'yet another line' >> data.txt
+next
